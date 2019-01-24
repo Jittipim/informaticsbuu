7a3441b062d734ef9a6ec50bef2f659e60e1ab01
Jittipim108 <36034824+Jittipim@users.noreply.github.com>
2019-01-24 15:54:03 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = '5aUMYQBs5rMX2Sq4OcxmtJy5B2Z8SfWC/WhuusQ7qpy+8BDWfVilkJzYcxp7NActjwyv1wWWoCYjRMTjLi3YlkxxKCOb/jGRQfQBiJW+ae3sfeiabmvvKiiEyklYnauawCvWbRpsHtj6oez8C133bQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
