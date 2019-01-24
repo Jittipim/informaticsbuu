9734ca3fdf9fffcd1746d2ae123c00257f781713
Jittipim108 <36034824+Jittipim@users.noreply.github.com>
2019-01-24 15:52:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '5aUMYQBs5rMX2Sq4OcxmtJy5B2Z8SfWC/WhuusQ7qpy+8BDWfVilkJzYcxp7NActjwyv1wWWoCYjRMTjLi3YlkxxKCOb/jGRQfQBiJW+ae3sfeiabmvvKiiEyklYnauawCvWbRpsHtj6oez8C133bQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
